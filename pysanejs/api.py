21e5df842c81e6547ec075f6f83231007d818520
Raphaël Vinot <raphael@vinot.info>
2025-07-09 17:19:07 +0200

    new: Support for UA and proxies.

--- pysanejs/api.py
+++ pysanejs/api.py
@@ -2,7 +2,8 @@
 
 from __future__ import annotations
 
-from urllib.parse import urljoin
+from importlib.metadata import version
+from urllib.parse import urljoin, urlparse
 
 import requests
 
@@ -12,11 +13,26 @@ from requests.adapters import HTTPAdapter
 
 class SaneJS():
 
-    def __init__(self, root_url: str='https://sanejs.circl.lu/'):
-        self.root_url = root_url
+    def __init__(self, root_url: str | None = None, useragent: str | None = None,
+                 *, proxies: dict[str, str] | None = None):
+        '''QUery a SaneJS instance.
+
+        :param root_url: URL of the SaneJS instance, defaults to 'https://sanejs.circl.lu/'
+        :param useragent: User-Agent to use for requests.
+        :param proxies: Proxies to use for requests
+        '''
+        self.root_url = root_url if root_url else 'https://sanejs.circl.lu/'
+        if not urlparse(self.root_url).scheme:
+            self.root_url = 'http://' + self.root_url
+        if not self.root_url.endswith('/'):
+            self.root_url += '/'
+
         self.session = requests.session()
         retries = Retry(total=5, backoff_factor=.1, status_forcelist=[500, 502, 503, 504])
         self.session.mount('http://', HTTPAdapter(max_retries=retries))
+        self.session.headers['user-agent'] = useragent if useragent else f'PySaneJS / {version("pysanejs")}'
+        if proxies:
+            self.session.proxies.update(proxies)
 
     @property
     def is_up(self) -> bool:
